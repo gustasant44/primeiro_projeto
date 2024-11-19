6b57bf5fdb31368cc69cbaa13560ea7faf838091
gustasant44 <santos.gustavo.o2001@gmail.com>
2024-11-18 21:25:20 -0300

    loop.tuplas.py

---
+++ loop.py
@@ -0,0 +1,11 @@
+dicionario = {"nome":("erick")
+    ,
+    "idade":17,
+    "nome":"caique",
+    "idade":22 
+ }
+
+for chave,valor in dicionario.items(): 
+    print(f"{chave} {valor}")
+
+
---
+++ tuplas.py
@@ -0,0 +1,35 @@
+lista = ['banana' , 'maça', 'banana']
+
+
+
+
+def add_fruta(a, b):
+    lista.append(a)
+    lista.append(b)
+    print(lista)
+
+add_fruta("limão", "sorvete")
+
+def del_fruta():
+    lista.pop()
+    print(lista)
+
+del_fruta()    
+
+for x in lista: 
+    print(f"Esta fruta está na lista {x}")
+
+
+
+    for fruta in lista:
+    print (f"Os elementos contidos na lista são: {fruta}")
+
+lista = ['banana' , 'maça' , 'pera' , 'banana']
+tupla = (1,2,3,4,5,)
+meu_set = {1,2,3,4}
+dicionario = {"nome": "caique", "idade": 22}
+ 
+
+
+
+
